2fd31bbbe9fc5d6d77fa5116eab1da30fe38291a
Cobi <cobimoos100@gmail.com>
2016-02-18 21:57:21 -0500

    receiver email messages are received and displayed successfully with the help of sidekiq

---
+++ app/workers/email_receiver_worker.rb
@@ -0,0 +1,9 @@
+class EmailReceiverWorker
+  include Sidekiq::Worker
+
+  def perform(message)
+    mail = Mail::Message.new(message)
+
+    puts "New mail from #{mail.from.first}: #{mail.subject}"
+  end
+end
