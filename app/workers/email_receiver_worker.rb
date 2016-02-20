d1d69f4799678216c419cf8124d79d3090374d99
Cobi <cobimoos100@gmail.com>
2016-02-20 18:17:47 -0500

    Successfully saves any attachments from an email into rails app

--- app/workers/email_receiver_worker.rb
+++ app/workers/email_receiver_worker.rb
@@ -4,6 +4,17 @@ class EmailReceiverWorker
   def perform(message)
     mail = Mail::Message.new(message)
 
-    puts "New mail from #{mail.from.first}: #{mail.subject}, with attachments #{mail.attachments}, #{mail.attachment?}"
+    puts "New mail from #{mail.from.first}: #{mail.subject}, with attachments #{mail.attachments}, #{mail.has_attachments?}"
+
+
+    mail.attachments.each do |attch|
+      fn = attch.filename
+      puts fn
+      begin
+        File.open( fn, "w+b", 0644 ) { |f| f.write attch.body.decoded }
+        rescue Exception => e
+        logger.error "Unable to save data for #{fn} because #{e.message}"
+      end
+    end
   end
 end
